ac85a5b845d86d0856f2f4e18c5f30d842cd1159
kjoshi6 <kjoshipersonal@gmail.com>
2025-08-28 23:12:28 -0400

    Update custom-horoscope.js

--- custom-horoscope.js
+++ custom-horoscope.js
@@ -1,17 +1,17 @@
 // Custom Horoscope Data
 const horoscopes = {
-    aries: "Don't mumble your grievences to yourself - time to stand Up",
-    taurus: "Have one emotion, please...",
-    gemini: "Sit your ASS at HOME - nobody wants to play the mind games you started in your own head",
-    cancer: "There is no need to manipulate everyone with your tears",
-    leo: "You don't need to be asking the tough questions everyday.. the People are tired",
-    virgo: "The recieptionist at the dentist didn't smile at you because she hates her job not because she hates you",
-    libra: "When are you getting a personality that isn't people pleasing",
-    scorpio: "No need to be eyeing everyone like a little slut",
-    sagittarius: "You do not need to be terrorizing every room you walk into",
-    capricorn: "Enough money talk spend some time with your loved ones please",
-    aquarius: "Please face one emotion, emotionally, not logically.. we're all begging",
-    pisces: "Everyone wants to come live in lala land with you"
+    aries: "Don't mumble your grievences to yourself - time to stand Up", "wee wee wah wah no one cares...get over it" 
+    taurus: "It's okay to express your emotions, no one is going to get mad at you babe", "how does is it feel to be so funny, pretty, cool, and grounded all the time????" 
+    gemini: "Sit your ASS at HOME - nobody wants to play the mind games you started in your own head", "shut up stop talking about the same 2 things on repeat. please..." , "Everyone in your life is blessed to have such an easy going, witty, loyal, smartie pants on their side" , "the demons in your head are right... you are INSANE" 
+    cancer: "No need to manipulate everyone with your tears honey", 
+    leo: "You don't need to be asking the tough questions everyday.. the People are tired", "The world does actually revolve actually does revolve around you...somehow.."
+    virgo: "The recieptionist at the dentist didn't smile at you because she hates her job not because she hates you", 
+    libra: "When are you getting a personality that isn't people pleasing", "babe I'm looking for your backbone but I can't find it" , 
+    scorpio: "I can feel your intensity through the screen right now", "Tell someone a secret today" 
+    sagittarius: "You do not need to be terrorizing every room you walk into", 
+    capricorn: "Enough money talk spend some time with your loved ones please", "Get messy this weekend, you deserve it, as a little treat" 
+    aquarius: "Please face one emotion, emotionally, not logically.. we're all begging", "babe don’t listen to the haters, you are UNIQUE and DIFFERENT" , 
+    pisces: "Everyone wants to come live in lala land with you" , "
 };
 
 // Function to Display Horoscope
